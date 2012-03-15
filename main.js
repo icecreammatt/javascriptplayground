a26e9ae648add56ba62e6c9ba6bb5a2ebc68dc83
Matt Carrier <icecreammatt@gmail.com>
2012-03-15 05:24:22 +0000

    'Code for setting up canvas'

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log('Hello World');
+
+var config = {
+  canvasId: 'canvas',
+  width: 1280,
+  height: 720,
+  backgroundColor: '#CCC'
+};
+
+(function(){
+    var canvas,
+        context,
+        WIDTH,
+        HEIGHT,
+        backgroundColor;
+    
+    (function init() {
+        WIDTH = config.width;
+        HEIGHT = config.height;
+        canvas = document.getElementById(config.canvasId);
+        context = canvas.getContext('2d');
+        backgroundColor = config.backgroundColor;
+        
+        context.fillStyle = backgroundColor;
+        context.fillRect(0,0,WIDTH,HEIGHT);
+    }());
+    
+}(config));
