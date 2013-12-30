2b0acf074492e2f2b9b4a7c092f1d5dcc91da443
Matt Carrier <icecreammatt@gmail.com>
2013-12-30 02:13:33 -0800

    Added collision and partial physics

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ var g = (function(){
         backgroundColor,
         squareSize,
         grideLineSize,
-        players = []
+        players = [],
+        tiles = [];
 
     function init() {
         WIDTH = config.width;
@@ -27,9 +28,27 @@ var g = (function(){
         squareSize = config.squareSize;
         grideLineSize = config.grideLineSize;
 
+        createMap();
+
         clearScreen();
+
     };
     init();
+
+    function createMap() {
+      var rows = HEIGHT / squareSize;
+      var columns = WIDTH / squareSize;
+      var tileColumn = [];
+
+      for (var y = 0, len = rows; y < len; y++){
+        for (var x = 0, len = columns; x < len; x++){
+          tileColumn.push(Tile(x,y,'floor'));
+        };
+        tiles.push(tileColumn);
+        tileColumn = [];
+      };
+
+    };
 
     function clearScreen() {
       context.fillStyle = backgroundColor;
@@ -40,6 +59,16 @@ var g = (function(){
       context.fillStyle = color || 'blue';
       context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
     };
+
+    function drawCircle(x, y, color) {
+      context.beginPath();
+      context.arc(x * squareSize + squareSize/2, y * squareSize + squareSize/2, squareSize/2, 0, 2 * Math.PI, false);
+      context.fillStyle = color;
+      context.fill();
+      context.lineWidth = 1;
+      context.strokeStyle = color;
+      context.stroke();
+    }
 
     function drawGrid() {
       var rows = HEIGHT / squareSize;
@@ -60,13 +89,18 @@ var g = (function(){
     function render() {
       // Render all items on the screen
       clearScreen();
-      drawGrid();
+      //drawGrid();
+      drawTiles();
       drawPlayers();
     }
 
     function update() {
       // Update logic
       window.requestAnimationFrame(update);
+      players.forEach(function(player){
+        player.update();
+      });
+
       render();
     }
 
@@ -74,42 +108,235 @@ var g = (function(){
       update();
     }
 
+    function Tile(x,y,type) {
+
+      var _type = null,
+          _color = 'black',
+          _occupied = false,
+          _position = { x: 0, y: 0 };
+
+      setType(type);
+      _position.x = x || 0;
+      _position.y = y || 0;
+
+      function setOccupied(val) {
+        _occupied = val;
+      }
+
+      function isOccupied() {
+        return !!_occupied;
+      }
+
+      function setType(type) {
+        _type = type || 'floor';
+        _occupied = false;
+        switch (type) {
+          case 'wall':
+            _color = 'brown'
+            _occupied = true;
+            break;
+          case 'water':
+            _color = 'blue';
+            _occupied = true;
+            break;
+          case 'door':
+            _color = 'purple';
+            _occupied = true;
+            break;
+          case 'stairs':
+            _color = 'pink';
+            _occupied = true;
+            break;
+          case 'floor':
+            _color = 'gray';
+            _occupied = false;
+            break;
+          default:
+            _color = 'black';
+            _occupied = true;
+            break;
+          }
+      }
+
+      function getColor() {
+        return _occupied ? 'black' : _color;
+      }
+
+      function render() {
+        drawSquare(_position.x, _position.y, getColor());
+      }
+
+      return {
+        render: render,
+        type: _type,
+        position: _position,
+        setOccupied: setOccupied,
+        isOccupied: isOccupied
+      };
+    };
+
     function Character(name, color) {
       var position = { x: 0, y: 0 };
+
+      var type = null;
+      var currentPosition = null;
+      var velocity = 2;
+      var lastDirection = null;
+
+      var setPosition = function(x,y) {
+        if(canMove) {
+          if(!!currentPosition) {
+            currentPosition.setOccupied(false);
+          }
+          currentPosition = tiles[y][x];
+          currentPosition.setOccupied(true);
+
+          position.x = x;
+          position.y = y;
+        }
+
+      }
+
+      var render = function() {
+        // drawSquare(position.x, position.y, color);
+        drawCircle(position.x, position.y, color);
+      }
+
+      var update = function() {
+
+        //if bullet or velocity > 1
+        if(!!lastDirection && velocity > 1) {
+          var collisionPoint = getCollision(lastDirection);
+          move(lastDirection);
+          if(lastDirection === 'left' && position.y === collisionPoint.y) {
+            if(position.x <= collisionPoint.x) {
+              setPosition(collisionPoint.x, position.y);
+              // velocity = 1;
+            }
+          }
+          if(lastDirection === 'right') {
+            if(position.x >= collisionPoint.x && position.y === collisionPoint.y) {
+              setPosition(collisionPoint.x, position.y);
+              // velocity = 1;
+            }
+          }
+          if(lastDirection === 'down') {
+            if(position.y >= collisionPoint.y && position.x === collisionPoint.x) {
+              setPosition(position.x, collisionPoint.y);
+              velocity = 1;
+            }
+          }
+          if(lastDirection === 'up') {
+            if(position.y <= collisionPoint.y && position.x === collisionPoint.x) {
+              setPosition(position.x, collisionPoint.y);
+              velocity = 1;
+            }
+          }
+        }
+      }
+
+      var getPosition = function() {
+        return position;
+      }
+
+      var getCollision = function(direction) {
+        var xstart = position.x;
+        var ystart = position.y;
+
+        var moveX = 0;
+        var moveY = 0;
+
+        if(direction === 'up') {
+          moveY = -1;
+        }
+        else if(direction === 'down') {
+          moveY = 1;
+        }
+        else if(direction === 'left') {
+          moveX = -1;
+        }
+        else if(direction === 'right') {
+          moveX = 1;
+        }
+
+        var objectCanMove = true;
+        while(objectCanMove) {
+          objectCanMove = canMove(xstart += moveX, ystart += moveY);
+        }
+        return { x: xstart, y: ystart }
+      }
+
+      //assumes integer values
+      var canMove = function(x,y) {
+        var result = false;
+        if(position.y + y >= 0 &&
+           position.x + x >= 0 &&
+           position.y + y < tiles.length &&
+           !!tiles[position.y + y] &&
+           position.x + x < tiles[position.y + y].length &&
+           !!tiles[position.y + y][position.x + x]) {
+
+          // Move this logic out of canMove
+          var tile = tiles[position.y + y][position.x + x];
+          occupied = tile.isOccupied();
+          if(!occupied) {
+            return true;
+          }
+        }
+        return false;
+      }
 
       var move = function(direction) {
         switch (direction) {
           case 'up':
-            position.y -= 1;
+            if(canMove(0, -1 * velocity))
+              setPosition(position.x, position.y - 1 * velocity);
             break;
           case 'down':
-            position.y += 1;
+            if(canMove(0, 1 * velocity))
+              setPosition(position.x, position.y + 1 * velocity);
             break;
           case 'left':
-            position.x -= 1;
+            if(canMove(-1 * velocity, 0))
+              setPosition(position.x - 1 * velocity, position.y);
             break;
           case 'right':
-            position.x += 1;
+            if(canMove(1 * velocity, 0))
+              setPosition(position.x + 1 * velocity, position.y);
             break;
           default:
             break;
           }
+          lastDirection = direction;
       };
 
       return {
         name: name,
         color: color,
-        position: position,
-        move: move
+        setPosition: setPosition,
+        getPosition: getPosition,
+        render: render,
+        update: update,
+        move: move,
+        velocity: velocity
       }
     };
 
     function drawPlayers() {
       for(var i = 0; i < players.length; i++) {
-        drawSquare(players[i].position.x,
-          players[i].position.y,
-          players[i].color);
+        players[i].render();
       }
+    };
+
+    function drawTiles() {
+      var rows = HEIGHT / squareSize;
+      var columns = WIDTH / squareSize;
+
+      for (var y = 0, len = rows; y < len; y++){
+        for (var x = 0, len = columns; x < len; x++){
+          tiles[y][x].render();
+        };
+      };
     };
 
     function addPlayer(name, color) {
@@ -125,6 +352,7 @@ var g = (function(){
       update: update,
       addPlayer: addPlayer,
       players: players,
+      tiles: tiles,
       start: start
     };
 
@@ -138,11 +366,9 @@ g.addPlayer('chris', 'red');
 g.addPlayer('stephen', 'green');
 
 var matt = g.players[0];
-g.players[1].position.x = 12;
-g.players[1].position.y = 10;
+g.players[1].setPosition(12,10);
+g.players[2].setPosition(7,16);
 
-g.players[2].position.x = 7;
-g.players[2].position.y = 16;
 
 key('w', function() { matt.move('up'); });
 key('a', function() { matt.move('left'); });
